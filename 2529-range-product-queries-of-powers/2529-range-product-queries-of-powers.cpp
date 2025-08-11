3b2ccbd53cfff5c744cb05d5daed8901e8b253f1
JammyParth <112752956+JammyParth@users.noreply.github.com>
2025-08-11 14:16:32 +0530

    Time: 98 ms (10.64%), Space: 165.2 MB (27.66%) - LeetHub

---
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+
+
+    bool solve(vector<int>&powers ,int index, int n, vector<int>&realpowers){
+        
+    if(n < 0) return false;
+    if(n == 0) return true;
+    if (index == powers.size()) return false;
+
+        //include
+        realpowers.push_back(powers[index]);
+        if(solve(powers, index + 1, n - powers[index] ,realpowers)){
+            return true;
+        }
+        realpowers.pop_back();
+
+
+        //exclude
+        if (solve(powers, index + 1, n, realpowers)) {
+            return true; 
+        }
+
+    return false;
+
+    }
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        const int MOD = 1e9 + 7;
+        // vector<int> powers;
+        // vector<int> realpowers;
+        // //we have to build powers vector through pick and not pick.
+
+        // int p = 1;
+        // while (p <= n) {
+        //     powers.push_back(p);
+        //     p *= 2;
+        // }
+        // solve(powers, 0 , n , realpowers);
+
+
+        vector<int> realpowers;
+        for (int bit = 0; bit < 31; bit++) {
+            if (n & (1 << bit)) {
+                realpowers.push_back(1 << bit);
+            }
+        }
+
+        int m = realpowers.size();
+        vector<int> ans;
+
+        for(auto val : queries){
+            int start = val[0];
+            int end = min(val[1] , m - 1); //in range
+
+            long long prod = 1;
+
+            for(int i= start; i <= end; i++){
+                prod = (prod * realpowers[i]) % MOD;
+            }
+
+            ans.push_back((int)prod);
+        }
+
+    return ans;  
+    }
+};
