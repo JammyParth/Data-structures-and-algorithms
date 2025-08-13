7f502a1c1f317471ef225ad1d7367a108eefc3df
JammyParth <112752956+JammyParth@users.noreply.github.com>
2025-08-13 10:39:08 +0530

    Time: 14 ms (5.68%), Space: 9.3 MB (5.34%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -7,13 +7,18 @@ public:
         //81 = 1010001
         //either bit shifting
         if(n < 1) return false;
-        //or else using while
-        while(n % 3 == 0){
-            n /= 3;
+        // //or else using while
+        // while(n % 3 == 0){
+        //     n /= 3;
+        // }
+        // return n == 1;
+        int num = 0;
+        for(int i = 0; pow(3 , i) <= n; i++){
+            num = pow(3 , i);
+            if(num == n) return true;
         }
 
-
-        return n == 1;
+        return false;
 
     }
 };
