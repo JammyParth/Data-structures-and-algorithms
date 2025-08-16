8f953062cac279ada3e15659705790f9c2733560
JammyParth <112752956+JammyParth@users.noreply.github.com>
2025-08-16 19:56:21 +0530

    Time: 0 ms (100%), Space: 8.1 MB (12.18%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        vector<int> number;
-
-        while(num > 0){
-            int temp = num % 10;
-            number.push_back(temp);
-            num /= 10;
-        }
-        reverse(number.begin() , number.end());
+        
+        string number = to_string(num);
 
         for(int i = 0; i < number.size(); i++){
-            if(number[i] == 6){
-                number[i] = 9;
+            if(number[i] == '6'){
+                number[i] = '9';
                 break;
             }
         }
 
-        int ans = 0;
 
-        for(auto digit : number){
-            ans = ans * 10 + digit;
-        }
-
-        return ans;
+        return stoi(number);
     }
 };
