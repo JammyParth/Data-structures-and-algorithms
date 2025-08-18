2c15b8f0565fe3a74b0f9270773229ae4f66c3b3
JammyParth <112752956+JammyParth@users.noreply.github.com>
2025-08-18 20:25:03 +0530

    Time: 1045 ms (54.87%), Space: 256.8 MB (57.9%) - LeetHub

--- 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.cpp
+++ 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.cpp
@@ -1,31 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-
     int subarrayBitwiseORs(vector<int>& arr) {
-        // vector<int> temp;
-
-        // for(int i = 1; i < arr.size(); i++){
-        //     if(arr[i] != arr[i - 1]){
-        //         temp.push_back(arr[i]);
-        //     }
-        // }
         int n = arr.size();
-        set<int> results, prev, curr;
+        unordered_set<int> ans;        // stores all distinct ORs overall
+        unordered_set<int> prev, curr; // dp[i-1], dp[i]
 
-        for(int i= 0; i < n; i++){
+        for (int num : arr) {
+            curr.clear();
 
-                for(auto x : prev){
-                    curr.insert(arr[i] | x);
-                    results.insert(arr[i] | x);
-                }
+            // Subarray of length 1
+            curr.insert(num);
 
-                curr.insert(arr[i]);
-                results.insert(arr[i]);
-                prev = curr;
-                curr.clear();
+            // Extend previous subarrays
+            for (int val : prev) {
+                curr.insert(val | num);
+            }
+
+            // Add current results to answer
+            for (int val : curr) {
+                ans.insert(val);
+            }
+
+            prev = curr; // Move forward
         }
 
-        return results.size();
-
+        return ans.size();
     }
 };
