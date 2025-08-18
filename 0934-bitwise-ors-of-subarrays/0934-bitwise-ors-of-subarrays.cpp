12e9b90f6a865925222b5c9709e949a0bf27c6fb
JammyParth <112752956+JammyParth@users.noreply.github.com>
2025-08-18 20:42:46 +0530

    Time: 305 ms (90.55%), Space: 120.2 MB (91.34%) - LeetHub

--- 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.cpp
+++ 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.cpp
@@ -1,32 +1,51 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Solution {
+// public:
+//     int subarrayBitwiseORs(vector<int>& arr) {
+//         int n = arr.size();
+//         unordered_set<int> ans;        // stores all distinct ORs overall
+//         unordered_set<int> prev, curr; // dp[i-1], dp[i]
+
+//         for (int num : arr) {
+//             curr.clear();
+
+//             // Subarray of length 1
+//             curr.insert(num);
+
+//             // Extend previous subarrays
+//             for (int val : prev) {
+//                 curr.insert(val | num);
+//             }
+
+//             // Add current results to answer
+//             for (int val : curr) {
+//                 ans.insert(val);
+//             }
+
+//             prev = curr; // Move forward
+//         }
+
+//         return ans.size();
+//     }
+// };
+
+
 
 class Solution {
 public:
     int subarrayBitwiseORs(vector<int>& arr) {
         int n = arr.size();
-        unordered_set<int> ans;        // stores all distinct ORs overall
-        unordered_set<int> prev, curr; // dp[i-1], dp[i]
-
-        for (int num : arr) {
-            curr.clear();
-
-            // Subarray of length 1
-            curr.insert(num);
-
-            // Extend previous subarrays
-            for (int val : prev) {
-                curr.insert(val | num);
+        unordered_set<int> res;
+        for (int i = 0; i < n; i++) {
+            res.insert(arr[i]);
+            for (int j = i - 1; j >= 0; j--) {
+                if ((arr[i] | arr[j]) == arr[j]) break;
+                arr[j] |= arr[i];
+                res.insert(arr[j]);
             }
-
-            // Add current results to answer
-            for (int val : curr) {
-                ans.insert(val);
-            }
-
-            prev = curr; // Move forward
         }
-
-        return ans.size();
+        return res.size();
     }
 };
