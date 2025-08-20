8d65e92ccc4a09edec5a314e62ba77443345c6ed
JammyParth <112752956+JammyParth@users.noreply.github.com>
2025-08-20 14:41:53 +0530

    Time: 0 ms (100%), Space: 9.8 MB (66.26%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.cpp
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.cpp
@@ -6,9 +6,10 @@ public:
         vector<string> ans;
         for(int i = 0; i < size; i+=k){
 
-            if(i + k <= size){
+            if(i + k < size){
                 ans.push_back(s.substr(i , k));
             }
+
             else{
                 string last = s.substr(i);
                 last += string(k - last.size() , fill);
