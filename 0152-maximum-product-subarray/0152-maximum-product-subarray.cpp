a28b319bcd9bd5e72c27809883b888165bf99669
JammyParth <112752956+JammyParth@users.noreply.github.com>
2025-10-03 15:16:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        //prefix suffix method.
+        int n = nums.size();
+        vector<int> prefix(n , 1);
+
+        prefix[0] = nums[0];
+
+        for(int i = 1; i< n; i++){
+            prefix[i] = prefix[i - 1] * nums[i];
+        }
+
+        vector<int> suffix(n , 1);
+        suffix[n - 1] = nums[n - 1];
+
+        for(int i = n - 2; i >= 0; i--){
+            suffix[i] = suffix[i + 1] * nums[i];
+        }
+
+        int maxproduct = INT_MIN;
+
+        for(int i = 0; i <n; i++){
+            maxproduct = max(maxproduct , prefix[i]);
+            maxproduct = max(maxproduct, suffix[i]);
+        }
+
+
+    return maxproduct;
+    }
+};
