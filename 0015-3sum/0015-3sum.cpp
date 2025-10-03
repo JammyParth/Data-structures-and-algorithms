c3e49358e5d35b018de54d7483610d1faef93aeb
JammyParth <112752956+JammyParth@users.noreply.github.com>
2025-10-03 22:37:06 +0530

    Time: 65 ms (19.93%), Space: 31.1 MB (20.84%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,82 +1,48 @@
 class Solution {
 public:
-
-
     vector<vector<int>> threeSum(vector<int>& nums) {
+        
         vector<vector<int>> ans;
         int n = nums.size();
-        sort(nums.begin() , nums.end());
-    //     for(int i= 0; i<n; i++){
-    //         if(i > 0 && nums[i] == nums[i - 1]) continue;
-
-    //         int j = i + 1;
-    //         int k = n - 1;
-
-    //         while(j < k){
-    //         int sum = nums[i] + nums[j] + nums[k];
+        sort(nums.begin() ,  nums.end());
+        for(int i = 0; i < n; i++){
             
-    //         if(sum < 0){
-    //             j++;
-    //         }
-
-    //         else if(sum > 0){
-    //             k--;
-    //         }
-
-    //         else{
-    //             vector<int> temp = {nums[i] , nums[j] , nums[k]};
-    //             ans.push_back(temp);
-
-    //             j++;
-    //             k--;
-
-    //             while(j < k && nums[j] == nums[j - 1]) j++;
-    //             while(j < k && nums[k] == nums[k + 1]) k--;
-    //         }
-
-    //         }
-
-    //     }
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            } //skipping duplicates
 
 
-    // return ans;
+            int j = i + 1;
+            int k = n - 1;
 
-    for(int i =0; i < n; i++){
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
 
-        if(i > 0 && nums[i] == nums[i - 1]) continue; //skipping duplicates
+                if(sum < 0){
+                    j++;
+                }
 
-        int j = i + 1;
-        int k = n - 1;
+                else if(sum > 0){
+                    k--;
+                }
 
-        while(j < k){
+                else{
+                    vector<int> res;
+                    res = {nums[i] , nums[j] , nums[k]};
+                    ans.push_back(res);
 
-            int sum = nums[i] + nums[j] + nums[k];
+                    j++;
+                    k--;
 
-            if(sum < 0){
-                j++;
+                    while(j < n && nums[j] == nums[j - 1]) j++;
+                    while(k > j && nums[k] == nums[k + 1]) k--;
+
+                }
             }
 
-            else if(sum > 0){
-                k--;
-            }
-
-            else{
-
-                vector<int> temp = {nums[i] , nums[j] , nums[k]};
-                ans.push_back(temp);
-
-
-                j++;
-                k--;
-
-                while(j < k && nums[j] == nums[j - 1]) j++; //removing duplicates
-                while(j < k && nums[k] == nums[k + 1]) k--; 
-            }
-
+            
         }
 
-    }
-
-    return ans;
+        return ans;
     }
 };
