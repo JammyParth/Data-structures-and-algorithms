e3f804cb2001f62b5b7a11dad68f05ca97f7b5c1
JammyParth <112752956+JammyParth@users.noreply.github.com>
2025-10-03 22:17:09 +0530

    Time: 0 ms (100%), Space: 15.2 MB (65.95%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int start = 0;
-        int end = n - 1;
+            int start = 0, end = nums.size() - 1;
 
-        while(start <= end){
-            
+            while (start <= end) {
             int mid = start + (end - start) / 2;
 
-            if(target == nums[mid]){
-                return mid;
-            }
+            if (nums[mid] == target) return mid;
 
-
-            if(nums[start] <= nums[mid]){  
-
-                if(nums[start] <= target && target <= nums[mid]){
-                    end = mid - 1;
-                }
-
-                else{
-                    start = mid + 1;
+            // Check if left half is sorted
+            if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid - 1; // target in left half
+                } else {
+                    start = mid + 1; // target in right half
                 }
             }
-
-            else{
-
-                if(nums[mid + 1] <= target && target <= nums[end]){
-                    start = mid + 1;
-                }
-
-                else{
-                    end = mid - 1;
+            // Otherwise right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1; // target in right half
+                } else {
+                    end = mid - 1; // target in left half
                 }
             }
-
         }
-    return -1;
+        return -1;
+
     }
 };
