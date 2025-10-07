19314c1aafcd98c69a0e311af294ba0d44eb4cc6
JammyParth <112752956+JammyParth@users.noreply.github.com>
2025-10-07 22:12:20 +0530

    Time: 4 ms (48.13%), Space: 19.2 MB (10.5%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,50 +10,31 @@
  */
 class Solution {
 public:
-
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummyHead = new ListNode(0);
-        ListNode* merge = dummyHead;
-
-        while(list1 && list2){
-
-            if(list1 != NULL && list2 != NULL){
-                if(list1 -> val <= list2 -> val){
-                    merge -> next = list1;
-                    merge = list1;
-                    list1 = list1 -> next;
-                }
-
-                else{
-                    merge -> next = list2;
-                    merge = list2;
-                    list2 = list2 -> next;
-                }
-
-                //merge = merge -> next;
-            }
-
-        }
-
-        if(list1 != NULL){
-            merge -> next = list1;
-        }
-
-        if(list2 != NULL){
-            merge -> next = list2;
-        }
-
-        return dummyHead -> next;
-    }
-
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) return nullptr;
+        vector<int> values;
 
-        int n = lists.size();
-        for(int i = 1; i < n; i++){
-            lists[0] = mergeTwoLists(lists[0] , lists[i]);
+        for(auto list : lists){
+
+            while(list){
+                values.push_back(list -> val);
+                list = list -> next;
+            }
         }
 
-        return lists[0];
+        sort(values.begin() , values.end());
+
+
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
+
+        int n = values.size();
+        for(int i = 0; i < n; i++){
+
+            ListNode* newnode = new ListNode(values[i]);
+            curr -> next = newnode;
+            curr = curr -> next;
+        }
+
+        return dummy -> next;
     }
 };
