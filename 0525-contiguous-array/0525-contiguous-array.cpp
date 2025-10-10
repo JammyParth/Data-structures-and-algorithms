59fbd63232e6acc2ac741451b81f41ce225c59b3
JammyParth <112752956+JammyParth@users.noreply.github.com>
2025-10-10 22:36:55 +0530

    Time: 55 ms (5.17%), Space: 88.5 MB (24.45%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -6,7 +6,7 @@ public:
 
         int sum = 0;
         int maxsize = 0;
-        mp[0] = -1;
+        // mp[0] = -1;
         for(int i = 0; i < n; i++){
             if(nums[i] == 0){
                 sum -= 1;
