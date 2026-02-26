5f8d499a2c586aad27caf741854c68885cdd95dd
JammyParth <112752956+JammyParth@users.noreply.github.com>
2026-02-26 19:47:48 +0530

    Time: 0 ms (100%), Space: 7.8 MB (49.62%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-
     bool isPowerOfTwo(int n) {
 
-        // if(n == -2147483648) return false;
-
-
-        // int count = __builtin_popcount(n);
-        // return count == 1;
-
-        if(n == 1) return true;
-        if(n <= 0) return false;
-
-        while(n>2){
-            if(n % 2 == 1) return false;
-            n /= 2;
+        if(n < 0){
+            return false;
         }
-        return true;
+        int bits = __builtin_popcount(n);
+
+        if(bits == 1){
+            return true;
+        }
+
+        return false;
     }
 };
