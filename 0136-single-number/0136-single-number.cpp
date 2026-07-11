65911f74dba9843ca2037cfc69e12e3e133560b1
JammyParth <112752956+JammyParth@users.noreply.github.com>
2026-07-11 13:19:44 +0530

    Time: 0 ms (100%), Space: 20.7 MB (58.75%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = nums[0];
+
+        int n = nums.size();
+
+        for(int i = 1; i < n; i++){
+            res ^= nums[i];
+        }
+
+        return res;
+    }
+};
